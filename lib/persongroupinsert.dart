bcf5ab489a0c75af14d1f612f2b2271de137355c
pjh990909 <pjh990909@naver.com>
2024-04-30 14:59:56 +0900

    박종희

---
+++ lib/persongroupinsert.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/material.dart';
+
+class PersonGroupInsert extends StatelessWidget {
+  const PersonGroupInsert({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF0F0E36),
+        title: Text("그룹선택",style: TextStyle(fontSize: 18,color: Color(0xFF81D1FB)),),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back,color: Color(0xFF81D1FB),),
+          onPressed: () {
+            Navigator.of(context).pop(); // 뒤로가기 버튼 클릭 시 이전 화면으로 이동
+          },
+        ),
+      ),
+      body: Container(
+        height: 1000,
+        color: Color(0xFF0F0E36),
+        padding: EdgeInsets.fromLTRB(40,60,40,20),
+        child: _PersonGroupInsert(),
+      ),
+    );
+  }
+}
+class _PersonGroupInsert extends StatefulWidget {
+  const _PersonGroupInsert({super.key});
+
+  @override
+  State<_PersonGroupInsert> createState() => _PersonGroupInsertState();
+}
+
+class _PersonGroupInsertState extends State<_PersonGroupInsert> {
+
+
+  //변수
+  bool? _Null = false;
+  bool? _Friend = false;
+  bool? _Family = false;
+  bool? _aaaa = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 270,
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Color(0xFF161443),
+            borderRadius: BorderRadius.circular(20)
+          ),
+          child: Column(
+            children: [
+              Container(
+                width: 370,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Color(0xFF161443),
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Color(0xFF222457), // 테두리 색상
+                      width: 2, // 테두리 두께
+                    ),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Checkbox(
+                      checkColor: Color(0xFFffffff),
+                      shape:  RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      side: BorderSide(
+                        color: Color(0xFFDB5C5C),
+                        width: 2,
+                      ),
+                      value: _Null,
+                      onChanged: (value){
+                        print(value);
+                        setState(() {
+                          _Null = value;
+                        });
+                      }
+                    ),
+                    Text("지정 안함",style: TextStyle(color: Color(0xFFffffff)))
+                  ],
+                ),
+              ),
+              Container(
+                width: 370,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Color(0xFF161443),
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Color(0xFF222457), // 테두리 색상
+                      width: 2, // 테두리 두께
+                    ),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Checkbox(
+                      checkColor: Color(0xFFffffff),
+                      shape:  RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      side: BorderSide(
+                        color: Color(0xFFDB5C5C),
+                        width: 2,
+                      ),
+                      value: _Friend,
+                      onChanged: (value){
+                        print(value);
+                        setState(() {
+                          _Friend = value;
+                        });
+                      }
+                    ),
+                    Text("친구",style: TextStyle(color: Color(0xFFffffff)))
+                  ],
+                ),
+              ),
+              Container(
+                width: 370,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Color(0xFF161443),
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Color(0xFF222457), // 테두리 색상
+                      width: 2, // 테두리 두께
+                    ),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Checkbox(
+                      checkColor: Color(0xFFffffff),
+                      shape:  RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      side: BorderSide(
+                        color: Color(0xFFDB5C5C),
+                        width: 2,
+                      ),
+                      value: _Family,
+                      onChanged: (value){
+                        print(value);
+                        setState(() {
+                          _Family = value;
+                        });
+                      }
+                    ),
+                    Text("가족",style: TextStyle(color: Color(0xFFffffff)))
+                  ],
+                ),
+              ),
+              Container(
+                width: 370,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Color(0xFF161443),
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Color(0xFF222457), // 테두리 색상
+                      width: 2, // 테두리 두께
+                    ),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Checkbox(
+                      checkColor: Color(0xFFffffff),
+                      shape:  RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      side: BorderSide(
+                        color: Color(0xFFDB5C5C),
+                        width: 2,
+                      ),
+                      value: _aaaa,
+                      onChanged: (value){
+                        print(value);
+                        setState(() {
+                          _aaaa = value;
+                        });
+                      }
+                    ),
+                    Text("동료",style: TextStyle(color: Color(0xFFffffff)))
+                  ],
+                ),
+              ),
+              Container(
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF161443)
+                  ),
+                  onPressed: (){},
+                  child: Row(
+                    children: [
+                      Text("+",style: TextStyle(color: Color(0xFF06F004),fontSize: 20)),
+                      Container(
+                        margin: EdgeInsets.only(left: 10),
+                        child: Text("그룹추가(시간 남으면)",
+                            style: TextStyle(color: Color(0xFFffffff))))
+                    ],
+                  )
+
+
+                ),
+              ),
+            ],
+          ),
+        ),
+      ]
+    );
+  }
+}
---
+++ lib/personinsert.dart
@@ -0,0 +1,240 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PersonInsertForm extends StatelessWidget {
+  const PersonInsertForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color(0xFF0F0E36),
+        padding: EdgeInsets.fromLTRB(40,40,30,20),
+        child: _PersonInsertForm(),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 100,
+        child: BottomAppBar(
+          color: Color(0xFF0F0E36),
+          child: Row(
+            children: [
+              Container(
+                width: 160,
+                height: 80,
+                margin: EdgeInsets.only(left: 15),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF0F0E36)
+                  ),
+                  onPressed: (){},
+                  child: Text(
+                      "취소",
+                      style: TextStyle(color: Color(0xFF81D1FB),fontSize: 20)
+                  )
+                ),
+              ),
+              Container(
+                width: 160,
+                height: 80,
+                margin: EdgeInsets.only(left: 41),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF0F0E36)
+                  ),
+                  onPressed: (){},
+                  child: Text(
+                    "등록",
+                    style: TextStyle(color: Color(0xFF81D1FB),fontSize: 20)
+                  )
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+class _PersonInsertForm extends StatefulWidget {
+  const _PersonInsertForm({super.key});
+
+  @override
+  State<_PersonInsertForm> createState() => _PersonInsertFormState();
+}
+
+class _PersonInsertFormState extends State<_PersonInsertForm> {
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _hpController = TextEditingController();
+  final TextEditingController _genderController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _memoController = TextEditingController();
+
+  String men = "남성";
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Column(
+      children: [
+        Center(
+          child: Container(
+            width: 130,
+            height: 130,
+            margin: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Color(0xFF55FFAD),
+              borderRadius: BorderRadius.circular(300),
+            )
+          )
+        ),
+        Container(
+          color: Color(0xFF161443),
+          margin: EdgeInsets.fromLTRB(0, 10, 10, 5),
+          child: TextFormField(
+            style: TextStyle(color: Color(0xFFffffff)),
+            controller: _nameController,
+            decoration: InputDecoration(
+              icon: Container(
+                width: 20,
+                margin: EdgeInsets.only(left: 10),
+                child: Icon(Icons.person,color: Color(0xFFffffff),),
+              ),
+              labelText:'이름',
+              labelStyle: TextStyle(color: Color(0xFFffffff)),
+              hintText: '이름을 입력하세요',
+              hintStyle: TextStyle(color: Color(0xFFffffff)),
+              border: InputBorder.none
+            ),
+          ),
+        ),
+        Container(
+          color: Color(0xFF161443),
+          margin: EdgeInsets.fromLTRB(0, 10, 10, 5),
+          child: TextFormField(
+            style: TextStyle(color: Color(0xFFffffff)),
+            controller: _hpController,
+            decoration: InputDecoration(
+                icon: Container(
+                  width: 20,
+                  margin: EdgeInsets.only(left: 10),
+                  child: Icon(Icons.call,color: Color(0xFFffffff),),
+                ),
+                labelText:'전화번호',
+                labelStyle: TextStyle(color: Color(0xFFffffff)),
+                hintText: '전화번호를 입력하세요',
+                hintStyle: TextStyle(color: Color(0xFFffffff)),
+                border: InputBorder.none
+            ),
+          ),
+        ),
+
+        Container(
+          height: 60,
+          color: Color(0xFF161443),
+          margin: EdgeInsets.fromLTRB(0, 10, 10, 5),
+          child: Row(
+            children: [
+              Container(
+                width: 20,
+                margin: EdgeInsets.only(left: 10),
+                child: Icon(Icons.wc,color: Color(0xFFffffff),),
+              ),
+              /*
+              RadioListTile(
+                title: Text('남성'),
+                value: men,
+                groupValue: _genderController.text,
+                onChanged: (value){
+                  Text("sss");
+                  _genderController.text = men;
+                },
+              )
+              */
+            ]
+          ),
+        ),
+
+        Container(
+          color: Color(0xFF161443),
+          margin: EdgeInsets.fromLTRB(0, 10, 10, 5),
+          child: TextFormField(
+            style: TextStyle(color: Color(0xFFffffff)),
+            controller: _emailController,
+            decoration: InputDecoration(
+                icon: Container(
+                  width: 20,
+                  margin: EdgeInsets.only(left: 10),
+                  child: Icon(Icons.mail,color: Color(0xFFffffff),),
+                ),
+                labelText:'Email',
+                labelStyle: TextStyle(color: Color(0xFFffffff)),
+                hintText: 'Email을 입력하세요',
+                hintStyle: TextStyle(color: Color(0xFFffffff)),
+                border: InputBorder.none
+            ),
+          ),
+        ),
+        Container(
+          height: 60,
+          margin: EdgeInsets.fromLTRB(0, 10, 10, 5),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFF161443),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.zero
+              ),
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              padding: EdgeInsets.only(left:13),
+            ),
+            onPressed: (){
+              print("클릭");
+              Navigator.pushNamed(context, '/persongroupinsert');
+            },
+            child: Row(
+              children: [
+                Container(
+                  child: Icon(Icons.group,color: Color(0xFFffffff),),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 10),
+                  child: Text(
+                    "그룹",
+                    style: TextStyle(
+                      color: Color(0xFFffffff),
+                      fontSize: 16
+                    ),
+                  )
+                )
+              ],
+            ),
+          ),
+        ),
+        Container(
+          color: Color(0xFF161443),
+          height: 200,
+          margin: EdgeInsets.fromLTRB(0, 10, 10, 5),
+          child: TextField(
+            style: TextStyle(color: Color(0xFFffffff)),
+            controller: _memoController,
+            keyboardType: TextInputType.multiline,
+            maxLines: null,
+            decoration: InputDecoration(
+                icon: Container(
+                  width: 20,
+                  margin: EdgeInsets.only(left: 10),
+                  child: Icon(Icons.edit_document,color: Color(0xFFffffff),),
+                ),
+                labelText:'메모',
+                labelStyle: TextStyle(color: Color(0xFFffffff)),
+                hintText: '메모를 입력하세요',
+                hintStyle: TextStyle(color: Color(0xFFffffff)),
+                border: InputBorder.none
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
